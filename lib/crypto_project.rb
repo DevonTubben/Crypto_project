c9d5912a4ad0f0138b86f2487b8963164bfa4e86
Devon Tubben <tubbend1@msu.edu>
2021-01-04 14:17:09 -0500

    first commit

---
+++ lib/crypto_project.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "crypto_project/version"
+
+module CryptoProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/crypto_project/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CryptoProject
+  VERSION = "0.1.0"
+end
